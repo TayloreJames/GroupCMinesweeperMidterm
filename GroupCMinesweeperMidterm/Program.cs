b3523e47dd21ec69c2ed5d9981857215440b0700
TayloreJames <67704935+TayloreJames@users.noreply.github.com>
2020-08-17 19:15:19 -0400

    Initialize project

---
+++ GroupCMinesweeperMidterm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GroupCMinesweeperMidterm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
