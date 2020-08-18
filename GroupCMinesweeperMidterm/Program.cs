5bfca46a9c5551273a996ca12123ca62ecf00edc
TayloreJames <67704935+TayloreJames@users.noreply.github.com>
2020-08-17 22:00:46 -0400

    First group commit

--- GroupCMinesweeperMidterm/Program.cs
+++ GroupCMinesweeperMidterm/Program.cs
@@ -6,9 +6,22 @@ namespace GroupCMinesweeperMidterm
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Minesweeper!!");
+            Console.WriteLine("Please select the size of your board: ");
+            Console.ReadLine();
+
+            //You user input to select game board size - Taylore
+
+            //Ask what you want to do next (flag or uncover) - Jennifer
+
+
+            
             // Jennifer was here
             //Sid was here
+
         }
+
+
+
     }
 }
