88f5bb2b446735cddc79857fc3f06fe76a64b36e
Jennifer <jebalas7@gmail.com>
2020-08-19 18:38:46 -0400

    Jennifer add Methods for UserInput and play choices

--- GroupCMinesweeperMidterm/Program.cs
+++ GroupCMinesweeperMidterm/Program.cs
@@ -13,15 +13,76 @@ namespace GroupCMinesweeperMidterm
             //You user input to select game board size - Taylore
 
             //Ask what you want to do next (flag or uncover) - Jennifer
+            bool continuePlay = true;
+            do
+            {
+                GetUserNextPlay();
+            } while (continuePlay);
+        } //end Main
 
+        private static void GetUserNextPlay()
+        {
+            Console.WriteLine("What is your next move? ");
+            Console.WriteLine("Do you want to: ");
+            Console.WriteLine("1) Uncover a cell");
+            Console.WriteLine("2) Flag a cell");
+            Console.WriteLine("3) Remove a flag");
+            bool inputValid = true;
+            do
+            {
+                Console.Write("Please enter 1, 2 or 3: ");
+                var userChoice = int.TryParse(Console.ReadLine(), out int playChoice);
+                if (!userChoice || playChoice < 1 || playChoice > 3)
+                {
+                    inputValid = false;
+                    Console.WriteLine("You have entered an invalid choice. Please try again! ");
+                } //end if
+                else
+                {
+                    switch (playChoice)
+                    {
+                        case 1:
+                            UncoverCell();
+                            break;
+                        case 2:
+                            FlagCell();
+                            break;
+                        case 3:
+                            RemoveFlag();
+                            break;
+                    }//end switch
+                }//end else
+            } while (!inputValid);
+        }
 
-            
-            // Jennifer was here
-            //Sid was here
+        public static void UncoverCell()
+        {
+            Console.WriteLine("What cell would you like to uncover? ");
+            Console.ReadLine();
+            if(bombPresent = true)
+            {
+                Console.WriteLine("Game Over");
+                continuePlay = false;
+                DisplayBoard();
+            }
+            else
+            {
+                if (numBombs = 0)
+                {
+
+                }
+            }
+        }
+
+        public static void FlagCell()
+        {
 
         }
 
+        public static void RemoveFlag()
+        {
 
+        }
 
-    }
-}
+    }//end class Program
+} //end Namespace
